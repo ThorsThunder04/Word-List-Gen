e8c89705e0952c6d488f41908296f85fbdb41129
sanzelda <thordahl04@gmail.com>
2021-08-06 17:27:53 +0200

    Initial commit

---
+++ wordListGenerator.py
@@ -0,0 +1,6 @@
+"""
+Written entirely by Thor.N
+
+This program will generate a word list to use in password cracking.
+You will input the characters you want in the word list and the minimum and maximum length of each word.
+"""
